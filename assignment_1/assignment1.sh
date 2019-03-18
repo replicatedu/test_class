91da79a4bc16ceca8ecd728f1de8dfafc218f65b
alex <hortinstein@gmail.com>
2019-03-17 22:05:55 -0400

    updated class

---
+++ assignment_1/assignment1.sh
@@ -0,0 +1,9 @@
+   #///!_SKELETON
+   #//!_ #change the below code to print "hello world1"
+   #//!_ echo "change me"
+   #///!_SKELETON
+
+   #///!_SOLUTION
+   # echos hello world
+   echo "hello world1";
+   #///!_SOLUTION
---
+++ assignment_3/assignment3.sh
@@ -0,0 +1,9 @@
+   #///!_SKELETON
+   #//!_ #change the below code to print "hello world3"
+   #//!_ echo "change me"
+   #///!_SKELETON
+
+   #///!_SOLUTION
+   # echos hello world
+   echo "hello world3";
+   #///!_SOLUTION
